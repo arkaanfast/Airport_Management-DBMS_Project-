e727648410805c885753d3fec5834f95dead7b32
arkaanfast <4pa17cs011@pace.edu.in>
2019-10-26 11:42:51 +0530

    updated

--- bookticket/admin.py
+++ bookticket/admin.py
@@ -3,3 +3,6 @@ from . import models
 # Register your models here.
 admin.site.register(models.Airport)
 admin.site.register(models.FlightDetails)
+admin.site.register(models.Booking)
+admin.site.register(models.Passenger)
+admin.site.register(models.Employee)
--- bookticket/models.py
+++ bookticket/models.py
@@ -23,3 +23,37 @@ class FlightDetails(models.Model):
 
     def __str__(self):
         return self.flight_name
+
+
+class Booking(models.Model):
+
+    booking_id = models.IntegerField(primary_key=True)
+    confirmation = models.CharField(max_length=4)
+
+
+class Passenger(models.Model):
+
+    from_airport = models.ForeignKey(Airport, related_name="from_airport", on_delete=models.CASCADE)
+    to_airport = models.ForeignKey(Airport, related_name="to_airport", on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    passport_no = models.CharField(max_length=10, primary_key=True)
+    email = models.EmailField(max_length=30)
+    flight_id = models.ForeignKey(FlightDetails, on_delete=models.CASCADE)
+    booking_id = models.ForeignKey(Booking, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+
+    emp_id = models.IntegerField(primary_key=True)
+    emp_name = models.CharField(max_length=20)
+    salary = models.FloatField()
+    duty = models.CharField(max_length=40, null=False)
+
+    def __str__(self):
+        return self.emp_name
+
+
+
