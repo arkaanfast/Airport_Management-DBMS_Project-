91b748e5b3cb6c0ffc520ad258faa196f5d20ac8
arkaanfast <4pa17cs011@pace.edu.in>
2019-10-26 10:39:40 +0530

    more

--- bookticket/admin.py
+++ bookticket/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Airport)
+admin.site.register(models.FlightDetails)
--- bookticket/models.py
+++ bookticket/models.py
@@ -2,5 +2,24 @@ from django.db import models
 
 # Create your models here.
 
+
 class Airport(models.Model):
 
+    airport_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class FlightDetails(models.Model):
+
+    flight_id = models.IntegerField(primary_key=True)
+    flight_name = models.CharField(max_length=50)
+    from_airport = models.ForeignKey(Airport, related_name="from_airport_id", on_delete=models.CASCADE)
+    time_arrival = models.DateTimeField()
+    time_departure = models.DateTimeField()
+    to_airport = models.ForeignKey(Airport, related_name="to_airport_id", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.flight_name
